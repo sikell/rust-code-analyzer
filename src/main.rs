9d10d02d28d51667a5dea4921f0ad50faa42c45e
Simon Keller <simkelle@htwg-konstanz.de>
2018-05-20 13:16:54 +0200

    use walkdir library to list all files recursively in directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
- fn main() {
-    println!("Hello, world!");
+extern crate walkdir;
+
+use walkdir::WalkDir;
+
+fn main() {
+    let analyzed_repo_path = "./";
+    
+    for entry in WalkDir::new(analyzed_repo_path) {
+        let entry = entry.unwrap();
+        println!("{}", entry.path().display());
+    }
 }
