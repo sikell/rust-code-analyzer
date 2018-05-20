103105afffb5b604a12e9f9f05f54aba17a476cf
Simon Keller <simkelle@htwg-konstanz.de>
2018-05-20 14:16:01 +0200

    sum total lines of all code files specified in properties

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,57 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use walkdir::WalkDir;
 use std::path::Path;
+use walkdir::DirEntry;
+use walkdir::Error;
+use std::collections::LinkedList;
+
+struct Properties<'a> {
+    analyzed_repo_path: &'a str,
+    file_extension: &'a str,
+}
+
+struct FileReport {
+    total_lines: usize
+}
 
 fn main() {
-    let analyzed_repo_path = "./";
-    let file_extension = ".rs";
+    let properties = Properties {
+        analyzed_repo_path: "./",
+        file_extension: ".rs",
+    };
 
-    for entry in WalkDir::new(analyzed_repo_path) {
-        let entry = entry.unwrap();
-        let name = match entry.file_name().to_str() {
-            None => "???",
-            Some(t) => t
-        };
-        if name.ends_with(file_extension) {
-            println!("{}", entry.path().display());
-            open_file(entry.path())
-        }
+    let report: LinkedList<FileReport> = WalkDir::new(properties.analyzed_repo_path)
+        .into_iter()
+        .map(|e| process_entry(&properties, e))
+        .filter(|e| e.is_some())
+        .map(|e| e.unwrap())
+        .collect();
+
+    let total_lines: usize = report.into_iter()
+        .map(|e| e.total_lines)
+        .sum();
+    println!("======\nTotal lines: {}", total_lines)
+}
+
+fn process_entry(properties: &Properties, entry: Result<DirEntry, Error>) -> Option<FileReport> {
+    let entry = entry.unwrap();
+    let name = match entry.file_name().to_str() {
+        None => "???",
+        Some(t) => t
+    };
+    if name.ends_with(properties.file_extension) {
+        return Some(open_file(entry.path()));
+    } else {
+        None
     }
 }
 
-fn open_file(file_path: &Path) {
+fn open_file(file_path: &Path) -> FileReport {
     let file = File::open(file_path).unwrap();
     let lines = BufReader::new(file).lines();
-    println!("{} lines in {}", lines.count(), file_path.display());
+    let total_lines = lines.count();
+    println!("{} lines in {}", total_lines, file_path.display());
+    FileReport {
+        total_lines
+    }
 }
