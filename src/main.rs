ea4043ea358d2361109493372f3bdc63e2611337
Simon Keller <simkelle@htwg-konstanz.de>
2018-05-20 13:32:38 +0200

    filter files by file name extension

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use walkdir::WalkDir;
 
 fn main() {
     let analyzed_repo_path = "./";
-    
+    let file_extension = ".rs";
+
     for entry in WalkDir::new(analyzed_repo_path) {
         let entry = entry.unwrap();
-        println!("{}", entry.path().display());
+        let name = match entry.file_name().to_str() {
+            None => "???",
+            Some(t) => t
+        };
+        if name.ends_with(file_extension) {
+            println!("{}", entry.path().display());
+        }
     }
 }
